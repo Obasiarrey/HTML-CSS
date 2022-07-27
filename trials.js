5073a1bcd30f83a995f4f9077d790c13bf8ecec7
Obasiarrey <obasiarreynjok@gmail.com>
2022-07-27 02:24:02 +0100

    Homepage commit

---
+++ trials.js
@@ -0,0 +1,14 @@
+//to store a value
+localStorage.setItem("key1", "value1");
+//this will print the value
+alert(localStorage.getItem("key1"));
+//Removing a value
+localStorage.removeItem("key1");
+//removing all values
+localStorage.clear();
+//geolocation
+navigator.geolocation.getCurrentPosition();
+//working with canvas
+CanvasRenderingContext2D.font="bold 22px Tahoma";
+CanvasRenderingContext2D.textAlign="start";
+CanvasRenderingContext2D.filltext("start", 10, 30);
